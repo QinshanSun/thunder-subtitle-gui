c998f1c61a803fc276d730186e5c459dcfa70a89
QinshanSun <33112560+QinshanSun@users.noreply.github.com>
2019-05-10 01:33:46 +0800

    Update GUI Code
    Update GUI Code

--- thunder_subtitle/main.py
+++ thunder_subtitle/main.py
@@ -8,43 +8,35 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.horizontal_group_box = QGroupBox()
         self.title = '字幕下载器'
         self.left = 10
         self.top = 10
         self.width = 640
         self.height = 480
+        self.horizontal_group_box = QGroupBox()
+        self.file_label = QLabel('选择文件:', self)
+        self.file_input = QLineEdit(self)
+        self.open_file_button = QPushButton("打开", self)
+        self.result_table = QTableWidget()
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-
-        # self.open_file_name_dialog()
-        # self.open_file_names_dialog()
-        # self.save_file_dialog()
-        self.create_horizontal_layout()
-
         self.create_vertical_layout()
         self.show()
 
-    def create_horizontal_layout(self):
-        layout = QHBoxLayout()
-        file_label = QLabel('选择文件:', self)
-        layout.addWidget(file_label)
-        file_input = QLineEdit(self)
-        layout.addWidget(file_input)
-        open_file_button = QPushButton("打开", self)
-        open_file_button.clicked.connect(self.open_file_name_dialog)
-        layout.addWidget(open_file_button)
-        self.horizontal_group_box.setLayout(layout)
-
     def create_vertical_layout(self):
-        layout = QVBoxLayout()
-        result_table = QTableWidget()
-        layout.addWidget(self.horizontal_group_box)
-        layout.addWidget(result_table)
-        self.setLayout(layout)
+        layout = QHBoxLayout()
+        layout.addWidget(self.file_label)
+        layout.addWidget(self.file_input)
+        self.open_file_button.clicked.connect(self.open_file_name_dialog)
+        layout.addWidget(self.open_file_button)
+        self.horizontal_group_box.setLayout(layout)
+        v_layout = QVBoxLayout()
+        v_layout.addWidget(self.horizontal_group_box)
+        v_layout.addWidget(self.result_table)
+        self.setLayout(v_layout)
 
     def open_file_name_dialog(self):
         options = QFileDialog.Options()
@@ -53,7 +45,8 @@ class App(QWidget):
                                                    "All Files (*);;Python Files (*.py)", options=options)
         if file_name:
             print(file_name)
-            self.horizontal_group_box.findChildren(QLineEdit)[0].setText(file_name)
+            #self.horizontal_group_box.findChildren(QLineEdit)[0].setText(file_name)
+            self.file_input.setText(file_name)
 
     def open_file_names_dialog(self):
         options = QFileDialog.Options()
