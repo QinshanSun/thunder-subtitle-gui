1b59fdf6bcda75fd61ff307592571671a7a1c31f
QinshanSun <33112560+QinshanSun@users.noreply.github.com>
2019-05-31 00:10:58 +0800

    Fix Some Issues
    Fix some issues

--- thunder_subtitle/main.py
+++ thunder_subtitle/main.py
@@ -2,11 +2,8 @@
 # coding: utf-8
 import sys
 
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QPushButton, QVBoxLayout, \
-    QGroupBox, QHBoxLayout, QLabel, QTableWidget, QTableWidgetItem, QHeaderView, QAbstractItemView, QDesktopWidget, \
-    QMenu
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFileDialog, QPushButton, QVBoxLayout, \
+    QGroupBox, QHBoxLayout, QLabel, QTableWidget, QTableWidgetItem, QHeaderView, QAbstractItemView, QDesktopWidget
 
 from thunder_subtitle import thunder_subs
 from thunder_subtitle.search import get_url
@@ -26,7 +23,7 @@ class App(QWidget):
         self.file_input = QLineEdit(self)
         self.file_input.setPlaceholderText("选择视频文件")
         self.open_file_button = QPushButton("打开", self)
-        self.result_table = QTableWidget(10, 4, self)
+        self.result_table = QTableWidget(10, 5, self)
         self.init_ui()
 
     def init_ui(self):
@@ -55,6 +52,7 @@ class App(QWidget):
         self.result_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.result_table.cellDoubleClicked.connect(self.down_load_subtitle)
         self.result_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.result_table.setColumnHidden(4, True)
         v_layout.addWidget(self.horizontal_group_box)
         v_layout.addWidget(self.result_table)
         self.setLayout(v_layout)
@@ -91,25 +89,21 @@ class App(QWidget):
             vote_item = QTableWidgetItem(str(x['svote']))
             lan_item = QTableWidgetItem(x['language'])
             name_item = QTableWidgetItem(x['sname'])
-            name_item.setData(QtCore.Qt.UserRole, x['surl'])
-            # url_item = QTableWidgetItem(x['surl'])
+            url_item = QTableWidgetItem(x['surl'])
             self.result_table.setItem(i, 0, rate_item)
             self.result_table.setItem(i, 1, vote_item)
             self.result_table.setItem(i, 2, lan_item)
             self.result_table.setItem(i, 3, name_item)
-            # self.result_table.setItem(i, 4, url_item)
+            self.result_table.setItem(i, 4, url_item)
+            self.result_table.setColumnHidden(4, True)
 
     def down_load_subtitle(self, p_x, p_y):
-        print("double clicked")
-        print(p_x)
-        url_tem = self.result_table.item(p_x, 3)
+        url_tem = self.result_table.item(p_x, 4)
         movie_file_path_wo_ext = self.file_input.text().rsplit('.', 1)[0]
         if url_tem is not None:
-            print(url_tem.text())
-            url = url_tem.data
+            url = url_tem.text()
             data = get_url(url)
             sub_ext = url.rsplit('.', 1)[1]
-            print(data)
             sub_file_path = movie_file_path_wo_ext + '.' + sub_ext
             with open(sub_file_path, 'wb') as f:
                 f.write(data)
